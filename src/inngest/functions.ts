67014be16af4aaa7ac5f8730087616caf15942a1
Shubham Kumar Chaudhary <shubhamkumchaudhary@gmail.com>
2025-10-05 02:58:32 +0530

    05: added ai agent

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,19 @@
+import {openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
-
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "5s");
+  async ({ event}) => {
+     const codeAgent = createAgent({
+      name: "codeAgent",
+      system: "You are an expert next.js developer.  You write readable, maintainable code.  You write simple Next.js & React snippets.",
+      model: openai({ model: "gpt-4o"}),
+    });
+    const { output } = await codeAgent.run(
+    `Write the following snippets: ${event.data.value} `,
+   );
+console.log(output);
 
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+    return {output };
   },
 );
