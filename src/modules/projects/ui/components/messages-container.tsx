1af0c2f74c4a23f7ff01207251bc2d5a6dc2f808
Shubham Kumar Chaudhary <shubhamkumchaudhary@gmail.com>
2025-10-06 20:42:40 +0530

    homepage

--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -18,6 +18,7 @@ export const MessagesContainer = ({
     setActiveFragment,
        }: Props) => {
     const bottomRef = useRef<HTMLDivElement>(null);
+    const lastAssistantMessageIdRef = useRef<string|null>(null);
     const trpc =useTRPC();
    const {data: messages} = useSuspenseQuery(trpc.messages.getMany.queryOptions({
         projectId: projectId,
@@ -25,14 +26,13 @@ export const MessagesContainer = ({
         refetchInterval: 5000,
     }));
 
-    // useEffect(()=>{
-    //     const lastAssistantMessageWithFragment = messages.findLast(
-    //         (message)=>message.role==="ASSISTANT"&& !!message.fragment,
-    //     );
-    //     if(lastAssistantMessageWithFragment){
-    //        setActiveFragment(lastAssistantMessageWithFragment.fragment);
-    //     }
-    // },[messages, setActiveFragment]);
+    useEffect(()=>{
+     const lastAssistantMessage = messages.find((message)=>message.role==="ASSISTANT");
+     if(lastAssistantMessage?.fragment && lastAssistantMessage.id !== lastAssistantMessageIdRef.current){
+        setActiveFragment(lastAssistantMessage.fragment);
+        lastAssistantMessageIdRef.current = lastAssistantMessage.id;
+     }
+    },[messages, setActiveFragment]);
 
     useEffect(()=>{
         bottomRef.current?.scrollIntoView({
